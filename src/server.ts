9da06220ba1bced3c7953b82456dbbeceadd061f
mine0059 <onojefemuemine@gmail.com>
2026-03-24 11:21:56 +0100

    project setup

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+/**
+ * @copyright 2026 oghenemine emmanuel
+ * @license Apache-2.0
+ */
+
+import express from 'express';
+
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
